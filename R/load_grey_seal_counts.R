279c6350f8e6ca40e8a173dea9a00747ebf6ea6f
Agnes Olin <agnesolin@gmail.com>
2025-12-10 12:15:39 +0100

    new function to load seal data

---
+++ R/load_grey_seal_counts.R
@@ -0,0 +1,15 @@
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' cat_function()
+
+load_grey_seal_counts <- function(){
+
+    print("I love cats!")
+
+
+}
