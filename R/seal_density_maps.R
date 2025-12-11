e7bbe5288ad5f0c4f79063544575c63411cd7176
Agnes Olin <agnesolin@gmail.com>
2025-12-11 14:58:44 +0100

    fix issue with format when only one point

--- R/seal_density_maps.R
+++ R/seal_density_maps.R
@@ -19,23 +19,39 @@ extract_seal_density <- function(dataframe){
     map = terra::rast(paste0("//storage-ua.slu.se/research$/Aqua/Områdesskydd och havsplanering/seals-cormorants/grey-seal-maps-Floris/densityLayersNormalized/Predictions_" , y, "_normalized.tif"))
 
     # subset to data from this year
-    df.sub = subset(df, year == y)
+    df.sub = subset(dataframe, year == y)
 
     # fix coordinates to match map
     locs = sf::st_as_sf(df.sub, coords = c("long", "lat"), crs = 4326)
     locs = sf::st_transform(locs, terra::crs(map))
 
+    if(nrow(df.sub > 1)){
 
-    res = rbind(
-      res,
-      cbind(df.sub,
-            data.frame(pred_mean = terra::extract(map$pred_mean, locs, search_radius = 10000)[, c("pred_mean")]),
-            data.frame(pred_95CI_lower = terra::extract(map$pred_95CI_lower, locs, search_radius = 10000)[, c("pred_95CI_lower")]),
-            terra::extract(map$pred_95CI_upper, locs, search_radius = 10000)[, c("pred_95CI_upper", "distance")]
+      res = rbind(
+        res,
+        cbind(df.sub,
+              data.frame(pred_mean = terra::extract(map$pred_mean, locs, search_radius = 10000)[, c("pred_mean")]),
+              data.frame(pred_95CI_lower = terra::extract(map$pred_95CI_lower, locs, search_radius = 10000)[, c("pred_95CI_lower")]),
+              terra::extract(map$pred_95CI_upper, locs, search_radius = 10000)[, c("pred_95CI_upper", "distance")]
+        )
+      )
+    }
+    if(nrow(df.sub == 1)){
 
-            )
+      res = rbind(
+        res,
+        cbind(df.sub,
+              data.frame(pred_mean = terra::extract(map$pred_mean, locs, search_radius = 10000)[1, c("pred_mean")]),
+              data.frame(pred_95CI_lower = terra::extract(map$pred_95CI_lower, locs, search_radius = 10000)[1, c("pred_95CI_lower")]),
+              data.frame(pred_95CI_upper = terra::extract(map$pred_95CI_upper, locs, search_radius = 10000)[1, c("pred_95CI_upper")]),
+              data.frame(distance = terra::extract(map$pred_95CI_upper, locs, search_radius = 10000)[1, c("e[, 2:3]")])
+        )
+      )
 
-    )
+
+      }
+
+
   }
 
   return(res)
