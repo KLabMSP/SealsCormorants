9acd4f7d6916d19891ae65a99dc94a0427674050
Agnes Olin <agnesolin@gmail.com>
2025-12-18 13:41:22 +0100

    updates to predation index function

--- R/predation_index.R
+++ R/predation_index.R
@@ -18,12 +18,12 @@ predation_index <- function(dataframe.counts, dataframe.extract, species){
   if(sum(names(dataframe.extract) %in% c("long", "lat", "year")) != 3) return(print("The dataset for which to extract values has to contain the columns lat, long and year. Try again!"))
 
   # smooth paras
-  if(species == "cormorant") max_dist = 40000
+  if(species == "cormorant") max_dist = 30000
   if(species == "grey_seal") max_dist = 60000
   if(!(species %in% c("cormorant", "grey_seal"))) return(print("Species has to be 'cormorant' or 'grey_seal'. Try again!"))
 
 
-  sigma = max_dist/1.96
+  sigma = max_dist/		2.576 # 99% of data contained within
 
   land = terra::rast("//storage-ua.slu.se/research$/Aqua/Områdesskydd och havsplanering/GIS-filer/baltic_sea_bathymetry_database/BSBD_0.9.6_250m/BSBD_0.9.6_250m.tif")
   land = terra::crop(land, terra::ext(4.25e+06, 5.45e+06, 3.3e+06, 5e+06))
@@ -71,15 +71,25 @@ predation_index <- function(dataframe.counts, dataframe.extract, species){
         terra::values(land)[cell] = 1
 
       }
-      # calculate predation index
+
+      # density as a function of distance
       pred.index = dists
-      terra::values(pred.index) = (dnorm(terra::values(dists), 0, sigma)/dnorm(0, 0, sigma))*as.numeric(counts.y$count[l])
+      terra::values(pred.index) = (dnorm(terra::values(dists), 0, sigma)/dnorm(0, 0, sigma)) # function of distance
       pred.index[pred.index > max_dist] = 0 # always zero beyond max distance
 
 
-      # correct for amount of available water
-      wat.area = sum(terra::values(land)[terra::values(dists) <= max_dist], na.rm = T)*250*250
-      pred.index = pred.index/wat.area
+
+      # set land to NA
+      values(pred.index)[is.na(values(land))] = NA
+
+      # scaling factor by colony/haul-out count so that numbers add up
+      count.scaling = as.numeric(counts.y$count[l])/sum(values(pred.index), na.rm = T)
+      pred.index = (pred.index*count.scaling) # apply scaling
+
+
+      # recalculate to per m2
+      pred.index = pred.index/(250*250)
+
 
       # add values to total map
       tot = tot + pred.index
@@ -101,6 +111,10 @@ predation_index <- function(dataframe.counts, dataframe.extract, species){
     extract.res = rbind(extract.res, extract.y)
 
   }
+
+  # set land to NA
+  values(tot)[is.na(values(land))] = NA
+
 
   return(extract.res)
 
